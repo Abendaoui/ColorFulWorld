8fb27e24d690d25590c35a7326a5e8c961cd1373
Abendaoui <90851562+Abendaoui@users.noreply.github.com>
2022-03-30 22:53:28 +0000

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function dark(){
+    document.getElementById('back').style.backgroundColor = "#1b1b1b";
+    document.body.style.background = "#1b1b1b";
+    document.getElementById('mybutton').style.display = "none";
+    document.getElementById('btn').style.display = "block";
+}
+function light(){
+    document.getElementById('back').style.backgroundColor = "#fff";
+    document.body.style.background = "#fff";
+    document.getElementById('mybutton').style.display = "block";
+    document.getElementById('btn').style.display = "none";
+}
